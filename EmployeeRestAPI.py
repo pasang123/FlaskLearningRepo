dff924c73b2a10c1b6b350f8d0725f5c60c5a12e
pasang123 <pas.sherpa123@gmail.com>
2021-04-06 12:46:44 +0530

    Custom logger added

--- EmployeeRestAPI.py
+++ EmployeeRestAPI.py
@@ -5,6 +5,17 @@ from json import dumps
 import json
 import sqlite3
 import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+filehandler = logging.FileHandler("test.log")
+logger.addHandler(filehandler)
+formatter = logging.Formatter("%(asctime)s:%(name)s:%(message)s")
+filehandler.setFormatter(formatter)
+filehandler.setLevel(logging.ERROR)
+StreamHandler = logging.StreamHandler()
+logger.addHandler(StreamHandler)
+StreamHandler.setFormatter(formatter)
+# logging.basicConfig(filename="test.log", level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 
 app = Flask(__name__)
 api = Api(app)
@@ -49,5 +60,14 @@ api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
 
 
 if __name__ == '__main__':
-     logging.info("Inside main")
-     app.run(port='5002')
+    # logger.debug("Debug")
+    # logger.info("InfO")
+    # logger.warning("WarninG")
+    # logger.error("Error")
+    # logger.critical("Critical")
+
+    try:
+        y = 2/0
+    except ZeroDivisionError:
+        logger.exception("Zero Division Error")
+    app.run(port=5002)
